977d1f8c818c12cdeb2d750dd8ccb569cbb399d9
Rachita01 <rachitasinghal6@gmail.com>
2021-12-09 17:05:16 +0530

    Change avatar final code

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import './ProfilePage.css';
 import ProfileCard from './ProfileCard';
-const ProfilePage = () => {
-  return (
-    <div>
-      <div className="profile_wrap">
-        <img
-          src="https://media.istockphoto.com/vectors/female-avatar-profile-icon-round-woman-face-vector-id897379486"
-          className="profile"
-        />
-        <h2 className="hoverContent">
-          <ProfileCard />
-        </h2>
+import { ProfileData } from './ProfileData';
+class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: ProfileData,
+      id: '1',
+    };
+
+    this.setProfile = (id) => {
+      this.setState({
+        id: id,
+      });
+    };
+  }
+
+  render() {
+    const { profiles } = this.state;
+    const imgsrc = profiles.filter((profile) => profile.id === this.state.id);
+
+    return (
+      <div>
+        <div className="profile_wrap">
+          <div>
+            <img src={imgsrc[0].imag} className="profile" />
+          </div>
+          <h2 className="hoverContent">
+            <ProfileCard getImageId={this.setProfile} />
+          </h2>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProfilePage;
